94669db2eef7474a8ebe442ee2471bfc5d59117d
Sagar Chhetri <sagarchhetri@Sagars-MacBook-Air.local>
2025-07-24 09:09:09 +0545

    dos

--- tutorfinder_backend/routes/favouriteRoutes.js
+++ tutorfinder_backend/routes/favouriteRoutes.js
@@ -3,7 +3,8 @@ const router = express.Router();
 const { toggleFavourite, getFavourites } = require("../controllers/favouriteController");
 const authGuard = require("../middleware/authGuard");
 
-router.get("/toggle/:tutorId", authGuard, toggleFavourite);
+// Fixed: Changed from GET to POST for toggle operation
+router.post("/toggle/:tutorId", authGuard, toggleFavourite);
 router.get("/all", authGuard, getFavourites);
 
 module.exports = router;
